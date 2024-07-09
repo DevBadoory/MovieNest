3a1de8b49a051f9014cd423dea609073ccc11516
Badoory <devbadoory@gmail.com>
2024-07-09 15:24:41 +0300

    creating color mode switcher

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
 import SearchInput from "./SearchInput";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} />
       <SearchInput />
+      <ColorModeSwitch />
     </HStack>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import React from "react";
@@ -6,13 +6,15 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes.tsx";
+import theme from "./theme.ts";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RouterProvider router={router} />
         <ReactQueryDevtools />{" "}
       </QueryClientProvider>
