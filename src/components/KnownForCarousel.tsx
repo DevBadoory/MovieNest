0b2d22ae7fc61381aa019b167012be290815e370
Badoory <devbadoory@gmail.com>
2024-07-29 05:31:45 +0300

    Add Knowfor carousel for PersonDetailPage

---
+++ src/components/KnownForCarousel.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Image, SimpleGrid, useColorMode } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { PersonCastMember } from "../entities/PersonCredits";
+import { getImage } from "../services/img_path";
+
+interface Props {
+  shows: PersonCastMember[];
+}
+
+const KnownForCarousel = ({ shows }: Props) => {
+  const { colorMode } = useColorMode();
+  const color = colorMode === "light" ? "#000" : "";
+  return (
+    <Box mb={10}>
+      <SimpleGrid
+        gridAutoFlow="column"
+        gridAutoColumns={{
+          base: "150px",
+          sm: "180px",
+          md: "160px",
+        }}
+        gap="1rem"
+        overflowX="scroll"
+        scrollBehavior="smooth"
+        css={{
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+          "&:hover": {
+            overflowY: "auto",
+          },
+        }}
+      >
+        {shows?.map((show) => (
+          <Box mt={5} borderRadius="10px" key={show.id}>
+            <Link to={`/${show.media_type}/${show.id}`}>
+              <Image
+                height="220px"
+                width="100%"
+                objectFit="cover"
+                borderTopRadius="10px"
+                src={getImage(show.poster_path || "")}
+              />
+            </Link>
+            <Box py={1} textAlign="center">
+              <Button
+                as={Link}
+                to={`/${show.media_type}/${show.id}`}
+                variant="link"
+                fontSize="1rem"
+                fontWeight="700"
+                textAlign="center"
+                height="auto"
+                padding={0}
+                whiteSpace="normal"
+                lineHeight="1.2"
+                color={color}
+              >
+                {show.title}
+              </Button>
+            </Box>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default KnownForCarousel;
--- src/pages/PersonDetailPage.tsx
+++ src/pages/PersonDetailPage.tsx
@@ -1,6 +1,5 @@
 import {
   Box,
-  Divider,
   Grid,
   GridItem,
   HStack,
@@ -12,12 +11,19 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import KnownForCarousel from "../components/KnownForCarousel";
+import usePersonCredits from "../hooks/usePersonCredits";
 import usePersonDetails from "../hooks/usePersonDetails";
 import { getImage } from "../services/img_path";
 
 const PersonDetailPage = () => {
   const { id } = useParams();
   const { data: person, error, isLoading } = usePersonDetails(String(id));
+  const { data: credits } = usePersonCredits(String(id));
+
+  const knownFor = credits?.cast
+    .sort((a, b) => b.vote_count - a.vote_count)
+    .slice(0, 9);
 
   if (isLoading) return <Spinner />;
   if (error) throw error;
@@ -34,7 +40,6 @@ const PersonDetailPage = () => {
             src={getImage(person.profile_path || "")}
             borderRadius="md"
             boxShadow="lg"
-            fallbackSrc="path/to/placeholder-image.jpg" // Add a placeholder image
           />
         </GridItem>
         <GridItem>
@@ -53,39 +58,30 @@ const PersonDetailPage = () => {
             {person.deathday && (
               <Text color="gray.500">Died: {person.deathday}</Text>
             )}
-            <Text fontWeight="bold">
-              Known For: {person.known_for_department}
-            </Text>
             {person.biography && (
               <Box>
-                <Heading as="h2" size="md" mb={2}>
+                <Heading as="h2" size="md" my={2}>
                   Biography
                 </Heading>
                 <Text>{person.biography}</Text>
               </Box>
             )}
-            <Divider />
-            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4} width="100%">
-              {person.also_known_as.length > 0 && (
-                <Box>
-                  <Heading as="h3" size="sm" mb={2}>
-                    Also Known As
-                  </Heading>
-                  <VStack align="start">
-                    {person.also_known_as.map((name, index) => (
-                      <Text key={index}>{name}</Text>
-                    ))}
-                  </VStack>
-                </Box>
-              )}
-              <Box>
-                <Heading as="h3" size="sm" mb={2}>
-                  Popularity
+            {knownFor && (
+              <Box
+                mt={6}
+                maxWidth="1094px"
+                width={{
+                  base: "calc(100vw - 33px)",
+                  sm: "calc(100vw - 60px)",
+                  md: "calc(100vw - 410px)",
+                }}
+              >
+                <Heading as="h2" size="md">
+                  Known For
                 </Heading>
-                <Text>{person.popularity.toFixed(2)}</Text>
+                <KnownForCarousel shows={knownFor} />
               </Box>
-            </SimpleGrid>
-            <Divider />
+            )}
             <Box width="100%">
               <Heading as="h2" size="md" mb={2}>
                 External Links
@@ -118,6 +114,28 @@ const PersonDetailPage = () => {
           </VStack>
         </GridItem>
       </Grid>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4} width="100%">
+        {person.also_known_as.length > 0 && (
+          <Box>
+            <Heading as="h3" size="sm" mb={2}>
+              Also Known As
+            </Heading>
+            <VStack align="start">
+              {person.also_known_as.map((name, index) => (
+                <Text key={index}>{name}</Text>
+              ))}
+            </VStack>
+          </Box>
+        )}
+
+        <Text fontWeight="bold">Known For: {person.known_for_department}</Text>
+        <Box>
+          <Heading as="h3" size="sm" mb={2}>
+            Popularity
+          </Heading>
+          <Text>{person.popularity.toFixed(2)}</Text>
+        </Box>
+      </SimpleGrid>
     </Box>
   );
 };
