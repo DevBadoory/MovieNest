f288bf15d28800367ffee29340097bb138ed8786
Badoory <devbadoory@gmail.com>
2024-07-31 23:56:19 +0300

    Hide React Query Devtools in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes.tsx";
 import theme from "./theme.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RouterProvider router={router} />
-        <ReactQueryDevtools />{" "}
+        {process.env.NODE_ENV !== "production" && <ReactQueryDevtools />}
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
