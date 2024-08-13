c646748d7c6f3cfeb8e8c354628b5ecb9edc601f
Badoory <devbadoory@gmail.com>
2024-08-14 01:35:02 +0300

    Refactored MovieDetailPage to use new components

--- src/pages/MovieDetailtPage.tsx
+++ src/pages/MovieDetailtPage.tsx
@@ -1,24 +1,19 @@
 import {
   Box,
-  Divider,
-  Flex,
   Grid,
   GridItem,
-  HStack,
   Heading,
-  Image,
   SimpleGrid,
   Spinner,
   Text,
-  VStack,
+  useColorMode,
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import BackdropSection from "../components/BackdropSection";
 import CreditCarousel from "../components/CreditsCarousel";
-import ExpandableText from "../components/ExpandableText";
-import MetaCriticDetailPage from "../components/MetaCriticDetailPage"; // Import the new component
+import MovieInfo from "../components/MovieInfo";
 import useMovieCredits from "../hooks/useMovieCredits";
 import useMovieDetails from "../hooks/useMovieDetails";
-import { getImageW200, getImageW500 } from "../services/img_path";
 
 const MovieDetailPage = () => {
   const { id } = useParams();
@@ -26,110 +21,77 @@ const MovieDetailPage = () => {
   const { data } = useMovieCredits(String(id));
 
   const casts = data?.cast.slice(0, 8);
+  const { colorMode } = useColorMode();
+  const color = colorMode === "light" ? "#fff" : "#121212";
 
   if (isLoading) return <Spinner></Spinner>;
   if (error) throw error;
 
+  if (!movie) return <Text>Movie not found.</Text>;
   return (
     <Box>
-      <Grid templateColumns={{ base: "1fr", md: "300px 1fr" }} gap={7}>
+      <BackdropSection backdropPath={movie?.backdrop_path || "#000"}>
+        {" "}
+        <MovieInfo movie={movie} crew={data?.crew || []} />
+      </BackdropSection>
+      <Grid templateColumns={{ base: "1fr", md: "1fr 210px" }} gap={0} mt={8}>
         <GridItem>
-          <Image
-            margin="auto"
-            height="450px"
-            width="300px"
-            objectFit="cover"
-            src={getImageW500(movie?.poster_path || "")}
-            borderRadius="md"
-            boxShadow="lg"
-          />
+          {casts && (
+            <Box
+              maxWidth="1200"
+              width={{
+                base: "calc(100vw - 33px)",
+                sm: "calc(100vw - 66px)",
+                md: "calc(100vw - 290px)",
+              }}
+            >
+              <Heading as="h2" size="lg">
+                Top Build Cast
+              </Heading>
+              <CreditCarousel
+                casts={casts || []}
+                showId={movie?.id}
+                type={"movie"}
+              />
+            </Box>
+          )}
         </GridItem>
-        <GridItem>
-          <VStack align="start" spacing={4}>
-            <Heading as="h1" size="2xl" margin={{ base: "auto", md: "0" }}>
-              {movie?.title}
-            </Heading>
-            <HStack spacing={4} wrap="wrap" margin={{ base: "auto", md: "0" }}>
-              <Text color="gray.500">{movie?.release_date?.split("-")[0]}</Text>
-              <Text color="gray.500">
-                {movie?.genres?.map((genre) => genre.name).join(", ")}
-              </Text>
-              <Text color="gray.500">{`${movie?.runtime} min`}</Text>
-            </HStack>
-            <Flex align="center">
-              <MetaCriticDetailPage score={movie?.vote_average || 0} />
-              <Text ml={2} fontWeight="bold">
-                User Score
-              </Text>
-            </Flex>
-            <Text fontStyle="italic" color="gray.500">
-              {movie?.tagline}
-            </Text>
+        <GridItem boxShadow={`-19px 1px 20px 9px ${color}`} zIndex="1">
+          <SimpleGrid
+            columns={{ base: 1 }}
+            spacing={4}
+            width="100%"
+            gap={8}
+            paddingLeft="1rem"
+          >
             <Box>
-              <Heading as="h2" size="md" mb={2}>
-                Overview
+              <Heading as="h3" size="sm" mb={2}>
+                Original Title
               </Heading>
-              <ExpandableText children={movie?.overview} />{" "}
+              <Text>{movie?.original_title}</Text>
             </Box>
-            <Divider />
-            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4} width="100%">
-              <Box>
-                <Heading as="h3" size="sm" mb={2}>
-                  Original Title
-                </Heading>
-                <Text>{movie?.original_title}</Text>
-              </Box>
-              <Box>
-                <Heading as="h3" size="sm" mb={2}>
-                  Status
-                </Heading>
-                <Text>{movie?.status}</Text>
-              </Box>
-              <Box>
-                <Heading as="h3" size="sm" mb={2}>
-                  Budget
-                </Heading>
-                <Text>${movie?.budget?.toLocaleString()}</Text>
-              </Box>
-              <Box>
-                <Heading as="h3" size="sm" mb={2}>
-                  Revenue
-                </Heading>
-                <Text>${movie?.revenue?.toLocaleString()}</Text>
-              </Box>
-            </SimpleGrid>
-            <Divider />
-            <Box width="100%" boxShadow="0 2px 8px rgba(0, 0, 0, .1)">
-              <Heading as="h2" size="md" mb={2}>
-                Production Companies
+            <Box>
+              <Heading as="h3" size="sm" mb={2}>
+                Status
               </Heading>
-              <SimpleGrid
-                mt={5}
-                columns={{ base: 1, sm: 2, lg: 3 }}
-                spacing={4}
-              >
-                {movie?.production_companies?.map((company) => (
-                  <Box key={company.id} display="flex" alignItems="center">
-                    <Image
-                      src={getImageW200(company.logo_path || "")}
-                      alt={company.name}
-                      height="auto"
-                      width="4rem"
-                      backgroundColor="#fff"
-                      objectFit="contain"
-                      mr={2}
-                    />
-                    <Text fontSize="sm" lineHeight="normal">
-                      {company.name}
-                    </Text>
-                  </Box>
-                ))}
-              </SimpleGrid>
+              <Text>{movie?.status}</Text>
             </Box>
-          </VStack>
+            <Box>
+              <Heading as="h3" size="sm" mb={2}>
+                Budget
+              </Heading>
+              <Text>${movie?.budget?.toLocaleString()}</Text>
+            </Box>
+            <Box>
+              <Heading as="h3" size="sm" mb={2}>
+                Revenue
+              </Heading>
+              <Text>${movie?.revenue?.toLocaleString()}</Text>
+            </Box>
+          </SimpleGrid>
+          <Box width="100%" boxShadow="0 2px 8px rgba(0, 0, 0, .1)"></Box>
         </GridItem>
       </Grid>
-      <CreditCarousel casts={casts || []} showId={movie?.id} type={"movie"} />
     </Box>
   );
 };
