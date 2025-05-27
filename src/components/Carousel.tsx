f25a03979a06a65098244152ef71a6c980504aeb
Badr <devbadoory@gmail.com>
2025-05-28 01:58:38 +0300

    Add ScrollButton in Carousel for scrolling arrows

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,40 +1,47 @@
-import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { Box, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
+import { ReactNode, useRef } from "react";
+import ScrollButton from "./ScrollButton";
 
-interface props {
+interface Props {
   title?: string;
   children: ReactNode;
 }
 
-const Carousel = ({ title, children }: props) => {
+const Carousel = ({ title, children }: Props) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
   return (
     <Box mb={10}>
       <Heading mb={5}>{title}</Heading>
-      <SimpleGrid
-        gridAutoFlow="column"
-        gridAutoColumns={{
-          base: "50%",
-          sm: "40%",
-          md: "30%",
-          lg: "21%",
-          xl: "14%",
-        }}
-        gap="1rem"
-        overflowX="scroll"
-        scrollBehavior="smooth"
-        css={{
-          "&::-webkit-scrollbar": {
-            display: "none",
-          },
-          "-ms-overflow-style": "none",
-          "scrollbar-width": "none",
-          "&:hover": {
-            overflowY: "auto",
-          },
-        }}
-      >
-        {children}
-      </SimpleGrid>
+      <Flex align="center" position="relative">
+        <ScrollButton direction="left" scrollRef={scrollRef} />
+        <Box
+          ref={scrollRef}
+          overflowX="scroll"
+          scrollBehavior="smooth"
+          w="100%"
+          css={{
+            "&::-webkit-scrollbar": { display: "none" },
+            "-ms-overflow-style": "none",
+            "scrollbar-width": "none",
+          }}
+        >
+          <SimpleGrid
+            gridAutoFlow="column"
+            gridAutoColumns={{
+              base: "50%",
+              sm: "40%",
+              md: "30%",
+              lg: "21%",
+              xl: "14%",
+            }}
+            gap="1rem"
+          >
+            {children}
+          </SimpleGrid>
+        </Box>
+        <ScrollButton direction="right" scrollRef={scrollRef} />
+      </Flex>
     </Box>
   );
 };
